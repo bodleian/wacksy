79545482dce7a4749f4b19fa7bf1609dddecff49
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2025-09-02 15:17:14 +0100

    test: add json schema validation to integration tests, closes #50

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,11 @@
 use pretty_assertions::assert_eq;
-use std::{error::Error, fs, path::Path};
-use wacksy::indexer;
+use serde_json::Value;
+use std::{
+    error::Error,
+    fs::{self, File},
+    path::Path,
+};
+use wacksy::{datapackage, indexer};
 
 const WARC_PATH: &str = "tests/example.warc.gz";
 
@@ -26,23 +31,30 @@ fn create_pages_index() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     Ok(())
 }
 
-// the datapackage cannot be easily tested because it contains
-// a local timestamp, how do I mock this?
-// #[test]
-// fn create_datapackage() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
-//     let warc_file_path: &Path = Path::new(WARC_PATH);
-//     let index = indexer::index_file(warc_file_path)?;
-//     let warc_file = fs::read(warc_file_path)?;
+/// This test creates a datapackage and validates it against the
+/// Frictionless Datapackage Schema v1
+#[test]
+fn valid_datapackage_json_schema() -> Result<(), Box<dyn Error>> {
+    let warc_file_path: &Path = Path::new(WARC_PATH);
+    let index = indexer::Index::index_file(warc_file_path)?;
+    let data_package = datapackage::DataPackage::new(&warc_file_path, &index)?;
+    // here we are casting the datapackage to string, then back out to a JSON Value.
+    // is there a better way of doing this?
+    let data_package_string = serde_json::to_string(&data_package)?;
+    let instance: Value = serde_json::from_str(&data_package_string)?;
+    let schema: Value = serde_json::from_reader(File::open("tests/data-package-schema.json")?)?;
 
-//     let cdxj_index_bytes = index.0.to_string().into_bytes();
-//     let pages_index_bytes = index.1.to_string().into_bytes();
+    // Build & reuse (faster)
+    let validator = jsonschema::validator_for(&schema)?;
 
-//     let data_package =
-//         datapackage::DataPackage::new(&warc_file, &cdxj_index_bytes, &pages_index_bytes)?;
+    // Iterate over errors
+    for error in validator.iter_errors(&instance) {
+        eprintln!("Error: {error}");
+        eprintln!("Location: {}", error.instance_path);
+    }
 
-//     let generated_data_package = serde_json::to_string(&data_package)?;
-//     let example_data_package =
-//         fs::read_to_string(Path::new("tests/wacz_example/datapackage.json"))?;
-//     assert_eq!(generated_data_package, example_data_package);
-//     Ok(())
-// }
+    // Boolean result
+    assert!(jsonschema::draft4::is_valid(&schema, &instance));
+
+    Ok(())
+}
