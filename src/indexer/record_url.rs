a543e1e33f6549423e8481aefd25656d4db49453
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2025-10-12 14:29:41 +0100

    deps: remove surt-rs library
    this reverses commit 15d73c950dcf613fac19f4b8c251ddbfad2c1839

--- src/indexer/record_url.rs
+++ src/indexer/record_url.rs
@@ -1,8 +1,7 @@
 use crate::indexer::indexing_errors::IndexingError;
 use serde::Serialize;
 use std::fmt;
-use surt_rs::generate_surt;
-use url::Url;
+use url::{Position, Url};
 use warc::{BufferedBody, Record, WarcHeader};
 
 #[derive(Serialize)]
@@ -35,18 +34,45 @@ impl RecordUrl {
     ///
     /// Take a url and return a Sort-friendly URI Reordering Transform (SURT)
     /// formatted string. It is cast to lowercase when displayed. This function
-    /// relies on the [surt-rs](https://github.com/mijho/surt-rs) crate.
+    /// follows [the example in the CDXJ spec](https://specs.webrecorder.net/cdxj/0.1.0/#searchable-url),
+    /// which goes through the following steps:
+    /// 
+    /// 1. lowercasing the URL
+    /// 2. removing the protocol portion (HTTP or HTTPS)
+    /// 3. replacing the [host name](https://url.spec.whatwg.org/#hosts-(domains-and-ip-addresses)) portion
+    /// of the URL with a reversed, comma separated equivalent: `www.example.org`` becomes `org,example,www`
+    /// 4. adding a `)` separator
+    /// 5. adding the remaining portion of the URL (path and query)
+    /// 
+    /// This is fast and simple, but might not be completely compatible with
+    /// other SURT implementations.
     ///
     /// # Errors
     ///
     /// Returns a `RecordUrlError` as a wrapper for `url::ParseError`
     /// if there is any problem parsing the url.
     pub fn as_searchable_string(&self) -> Result<String, IndexingError> {
-        match generate_surt(self.0.as_str()) {
-            Ok(sorted_url) => return Ok(sorted_url),
-            Err(sorting_parse_error) => {
-                return Err(IndexingError::RecordUrlError(sorting_parse_error));
-            }
+        if let Some(host) = self.0.host_str() {
+            // split the host string into an array at each dot
+            let mut host_split: Vec<&str> = host.split('.').collect();
+
+            // reverse the order of the array
+            host_split.reverse();
+
+            // join the array back into a comma-separated string
+            let host_reversed = host_split.join(",");
+
+            // capture everything else on the end of the url
+            let url_path = &self.0[Position::BeforePath..];
+
+            // put it all together
+            return Ok(format!("{host_reversed}){url_path}"));
+        } else {
+            // print the full url here
+            let url = self.0.as_str();
+            return Err(IndexingError::ValueNotFound(format!(
+                "{url} does not have a host, unable to construct a searchable string"
+            )));
         }
     }
 }
@@ -93,6 +119,6 @@ mod tests {
             .as_searchable_string()
             .unwrap();
 
-        assert_eq!(surt_parsed_url, "review,thehtml)/04/ascii-bedroom-archive");
+        assert_eq!(surt_parsed_url, "review,thehtml)/04/ascii-bedroom-archive/");
     }
 }
