cb54287ddf529359e87fe4646e46a8a7aebf18a7
James Baker <2686083+jamesdbaker@users.noreply.github.com>
2026-04-21 15:24:59 +0100

    Fixes #82 - replaces \r\n line endings with \n when reading reference file in test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -7,13 +7,17 @@ use wacksy::{
 
 const WARC_PATH: &str = "tests/example.warc.gz";
 
+fn normalize_newlines(s: String) -> String {
+    s.replace("\r\n", "\n")
+}
+
 #[test]
 fn create_cdxj_index() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     let warc_file_path: &Path = Path::new(WARC_PATH);
     let index = indexer(warc_file_path);
     let generated_cdxj_index = to_cdxj_string(&index);
     let example_cdxj_index =
-        fs::read_to_string(Path::new("tests/wacz_example/indexes/index.cdxj"))?;
+        normalize_newlines(fs::read_to_string(Path::new("tests/wacz_example/indexes/index.cdxj"))?);
     assert_eq!(generated_cdxj_index, example_cdxj_index);
     Ok(())
 }
